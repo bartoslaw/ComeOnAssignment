35a4d07689a3366570a816a91d10f48578e3327f
Bart Smektau <bs.smektala@gmail.com>
2022-06-22 10:13:53 +0200

    Init commit

---
+++ Native/ComeOn Assignment/ComeOn_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComeOn_AssignmentApp.swift
+//  ComeOn Assignment
+//
+//  Created by Bartłomiej Smektała on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ComeOn_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
